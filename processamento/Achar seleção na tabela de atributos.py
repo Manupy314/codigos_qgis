f2ec0bea4b6c42b904c76e6063b9ec8da2b76203
Manupy314 <manupy314@gmail.com>
2026-02-19 13:40:42 -0300

    adaptações

--- processamento/Achar seleção na tabela de atributos.py
+++
@@ -1,8 +0,0 @@
-layer = iface.activeLayer()
-selecionadas = layer.selectedFeatures()
-
-if not selecionadas:
-    print("Nenhuma feição selecionada.")
-else:
-    for f in selecionadas:
-        print(f"ID interno: {f.id()}, atributos: {f.attributes()}")
